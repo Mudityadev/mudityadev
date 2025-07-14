f7e1372243cc34559636bad92652bca7ebfbebd1
Mudityadev <mudityadev@gmail.com>
2025-07-14 11:35:23 +0530

    Removed Weather Toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,9 +57,7 @@ export default function Navbar({ onGalleryClick, onLogoClick }: { onGalleryClick
             </Link>
             
             {/* Total Visits Counter - Desktop */}
-            <div className="hidden md:flex items-center">
-              <UserWeatherBadge />
-            </div>
+           
           </div>
 
           {/* Desktop Actions */}
